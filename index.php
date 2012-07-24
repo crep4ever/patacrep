e9d6a8ed5272d8ef8406983303c16d1d20b0f81f
Romain Goffe <romain.goffe@gmail.com>
2012-07-24 23:22:11 +0200

    default site language according to browser locale

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
-<?php
-  header('Location: http://www.patacrep.com/fr/'); //Adresse de la nouvelle page
-  header('HTTP/1.1 301 Moved Permanently'); //Code HTTP de redirection permanente
-  header('Status: 301 Moved Permanently'); //Doublon utile à certaines versions de PHP et serveurs
-  header('Content-Type: text/html; charset=UTF-8');
-  echo '<'.'?xml version="1.0" encoding="UTF-8"?'.'>'; //entête XML
+<?
+$lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+switch ($lang){
+   case "fr":
+     header("Location: fr/index.php");
+     break;
+    default:
+     header("Location: en/index.php");
+     break;
+}
 ?>
