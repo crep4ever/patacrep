e967ea945efd5475221ed353a1dddc6c5fc357cf
Romain Goffe <romain.goffe@gmail.com>
2012-07-24 23:23:52 +0200

    setup redirections

---
+++ data/commentaires/index.php
@@ -0,0 +1,7 @@
+<?php
+  header('Location: http://www.patacrep.com/fr/data/commentaires/');
+  header('HTTP/1.1 301 Moved Permanently');
+  header('Status: 301 Moved Permanently');
+  header('Content-Type: text/html; charset=UTF-8');
+  echo '<'.'?xml version="1.0" encoding="UTF-8"?'.'>';
+?>
